efe7fa35efa971f0a292120c8e38ef36788b0860
coline <colnatcoline@hotmail.com>
2016-08-17 15:29:14 +0200

    adding google

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817094124) do
+ActiveRecord::Schema.define(version: 20160817131507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20160817094124) do
     t.integer  "nb_of_participants"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 
