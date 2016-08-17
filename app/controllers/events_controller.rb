e58e1d889873a1efde289a846618a934e2b34276
Benjamin Lyphoudt <benjamin.lyphoudt@gmail.com>
2016-08-17 15:47:14 +0200

    actions edit and update for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [ :show ]
+  before_action :set_event, only: [ :show, :edit, :update ]
 
   def index
     @events = Event.all
@@ -20,6 +20,17 @@ class EventsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to event_path(@event)
+    else
+      render :edit
+    end
   end
 
   private
