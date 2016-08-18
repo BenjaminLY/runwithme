f61b29adc9f69714b588127fd5aac75d9394abe1
coline <colnatcoline@hotmail.com>
2016-08-18 13:56:31 +0200

    conflict of migration solved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818103814) do
+ActiveRecord::Schema.define(version: 20160818110358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,12 @@ ActiveRecord::Schema.define(version: 20160818103814) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "last_name"
+    t.string   "first_name"
+    t.string   "job_title"
+    t.string   "run_level"
+    t.string   "expectation"
+    t.text     "description"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
