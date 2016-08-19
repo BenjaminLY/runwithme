3469e59ef714da3c28fedc27798251598f07f6da
coline <colnatcoline@hotmail.com>
2016-08-19 12:55:00 +0200

    partial events list done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
-  has_many :events, through: :participations
+  has_many :events, dependent: :destroy
   has_many :participations, dependent: :nullify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_attachment :photo
+
+  def events_as_participant
+  	self.participations.map do |participation|
+  		participation.event
+  	end
+  end
 end
