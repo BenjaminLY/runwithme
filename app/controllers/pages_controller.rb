61fd96427299953686fcd4e2728216714297bd12
Benjamin Lyphoudt <benjamin.lyphoudt@gmail.com>
2016-08-30 18:15:54 +0200

    update of navbar and correct css file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
+  skip_before_action :set_current_user_activities, only: :home
   skip_after_action :verify_authorized, only: [ :home ]
 
   def home
