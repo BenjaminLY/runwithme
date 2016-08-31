a8fd52b9c887dcae6c644c46efeef57a38ec2192
Benjamin Lyphoudt <benjamin.lyphoudt@gmail.com>
2016-08-31 19:59:23 +0200

    add picto to form register and add buttons to form new event

---
+++ app/assets/javascripts/private_choice.js
@@ -0,0 +1,16 @@
+function setPrivateChoice() {
+  $('#colleagues-choice').hide();
+  $('#random-choice').hide();
+  $('#colleagues').click(function () {
+    $(this).addClass('private-choice');
+    $('#random').removeClass('private-choice');
+    $('#colleagues-choice').show();
+    $('#random-choice').hide();
+  });
+  $('#random').click(function () {
+    $(this).addClass('private-choice');
+    $('#colleagues').removeClass('private-choice');
+    $('#random-choice').show();
+    $('#colleagues-choice').hide();
+  });
+}
